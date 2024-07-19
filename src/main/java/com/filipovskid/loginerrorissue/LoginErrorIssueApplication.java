d0885f86c4dd4cc53101baa673388e47cdbfb44e
Darko Filipovski <Darko.Filipovski@netcetera.com>
2024-07-19 19:03:12 +0200

    Application setup

---
+++ src/main/java/com/filipovskid/loginerrorissue/LoginErrorIssueApplication.java
@@ -0,0 +1,13 @@
+package com.filipovskid.loginerrorissue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginErrorIssueApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoginErrorIssueApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/filipovskid/loginerrorissue/LoginErrorIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.filipovskid.loginerrorissue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginErrorIssueApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
